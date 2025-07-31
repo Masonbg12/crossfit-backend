c0d2e3c84c022cd57dfb9ff30d6ba4d3f1421b74
Masonbg12 <masonbg1206@gmail.com>
2025-07-31 15:05:27 -0500

    modified:   server.js

--- server.js
+++ server.js
@@ -218,4 +218,8 @@ setInterval(async () => {
 
 // Start the server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on ${HOST}:${PORT}`);
+});
