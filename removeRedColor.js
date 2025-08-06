b67f7538c4a95d5cefadb4a8795a1d47f52868dd
Masonbg12 <masonbg1206@gmail.com>
2025-08-06 16:44:46 -0500

    modified:   removeRedColor.js

--- removeRedColor.js
+++ removeRedColor.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 async function updateAllPosts() {
   const posts = await POST.find({});
   for (const post of posts) {
-    const cleanedContent = post.content.replace(/color:\s*red/gi, 'color:#000');
+    const cleanedContent = post.content.replace(/color\s*:\s*(red|#ff0000)\b/gi, 'color:#000');
     if (cleanedContent !== post.content) {
       post.content = cleanedContent;
       await post.save();
